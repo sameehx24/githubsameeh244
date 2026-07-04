9a9482420be097b95c965994b96c215b1dc12ff3
sameehx24 <sameehx24@gmail.com>
2026-07-04 19:12:35 +0530

    Modified panirku

--- test1/Mello.java
+++ test1/Mello.java
@@ -3,6 +3,7 @@ package githubsameeh244.test1;
 public class Mello {
     public static void main(String[] args) {
         System.out.println("Mian method is executed");
+        System.out.println("Hi hello.....");
     }
     
 }
